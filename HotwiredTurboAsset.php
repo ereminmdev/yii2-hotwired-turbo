b017cc0d5b9923ffe41ab9d46d60b9e3fa522ba4
Eremin Mikhail <ereminmdev@gmail.com>
2022-11-18 19:06:25 +0500

    Add script in head block

--- HotwiredTurboAsset.php
+++ HotwiredTurboAsset.php
@@ -3,13 +3,18 @@
 namespace ereminmdev\yii2\hotwired_turbo;
 
 use yii\web\AssetBundle;
+use yii\web\View;
 
 class HotwiredTurboAsset extends AssetBundle
 {
     public $sourcePath = '@npm/hotwired--turbo/dist';
 
     public $js = [
-        'turbo.es2017-esm.js',
-        //'turbo.es2017-umd.js',
+        'turbo.es2017-umd.js',
+        //'turbo.es2017-esm.js',
+    ];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
     ];
 }
